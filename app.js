23a7c73f947e83c65774cc1f0eade0401a8f5a51
ned98 <37303080+ned98@users.noreply.github.com>
2020-08-13 19:18:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,94 @@
-let inputVal = $(".input");
-let btnAdd = $(".btnAdd");
-let btnReset = $(".btnReset");
-let li = $("li");
+let btnAdd = document.getElementById("btnAdd");
+let btnReset = document.getElementById("btnReset");
+let ol = document.getElementById("ol");
+//let inputVal = $(".input");
+// let btnAdd = $(".btnAdd");
+// let btnReset = $(".btnReset");
+//let li = $("li");
 
 
-btnAdd.click(function () {
+// Create a new list item when clicking on the "Add" button
+btnAdd.addEventListener("click", function () {
 
-    if (inputVal.val() == '') return;
+    let input = $("input");
+    let inputVal = $("#input").val();
+
+    if (inputVal == '') return;
 
     else {
-        $('ol').prepend(`<li class="">${inputVal.val()}</li>`);
-        inputVal.val("");
+        let li = document.createElement("li");
+        let clearBtn = document.createTextNode("\u00D7");
+        let span = document.createElement("span");
+
+        span.classList.add("clear");
+        li.classList.add("li");
+        span.prepend(clearBtn);
+        li.prepend(inputVal);
+        li.prepend(span);
+
+        ol.appendChild(li);
+
+        input.val("");
+        //$('ol').prepend(`<li>${inputVal.val()}</li>`);
+        //inputVal.val("");
     }
+
 });
 
-li.click(function () {
+btnReset.addEventListener("click", function () {
 
-    if ($(this).hasClass('has')) {
-        $(this).removeClass("strike-text");
-        $(this).removeClass("has");
+    let list = document.getElementsByClassName("li");
+
+    // Remove the 1st element, until there is no 1st element
+    while (list[0]) {
+        list[0].parentNode.removeChild(list[0]);
     }
 
-    else if (!$(this).hasClass("has")) {
-        $(this).addClass("strike-text");
-        $(this).addClass("has");
-    }
 });
+
+ol.addEventListener("click", function (e) {
+
+    // Adds strike text if you click the 'li'
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle("strike-text");
+    }
+
+    // Removes the 'li' if you click the "x" button
+    if (e.target.classList.contains('clear')) {
+        e.target.parentElement.remove();
+    }
+
+});
+
+
+
+
+// li.addEventListener("click", function () {
+
+//     if ($(this).hasClass('has')) {
+//         $(this).removeClass("strike-text");
+//         $(this).removeClass("has");
+//     }
+
+//     else if (!$(this).hasClass("has")) {
+//         $(this).addClass("strike-text");
+//         $(this).addClass("has");
+//     }
+// });
+
+
+
+
+// btnAdd.click(function () {
+
+//     if (inputVal.val() == '') return;
+
+//     else {
+//         let li = document.createElement("li");
+//         li.prepend(inputVal.val);
+//         ol.appendChild(li);
+
+//         //$('ol').prepend(`<li>${inputVal.val()}</li>`);
+//         inputVal.val("");
+//     }
+// });
