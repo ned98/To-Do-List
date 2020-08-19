d51ae95bd13676e7d40a63470bec940c8726ce76
ned98 <37303080+ned98@users.noreply.github.com>
2020-08-19 20:21:59 +0300

    Add files via upload
    Basic JS To-Do List

--- app.js
+++ app.js
@@ -1,96 +1,59 @@
 //$(document).ready(function () {
 let btnAdd = $("#btnAdd");
-let ulTodo = $("#ul-todo");
-let input = $("input");
-let btnReset = $("#btnReset");
-let toggleCompleted = $("#h1-completed");
+let toggleCompleted = $("#div-h1-completed");
 
 toggleCompleted.hide();
 
-// let LIST = [];
-// let id;
-
-// let data = localStorage.getItem("todo");
-
-// if (data) {
-//     LIST = JSON.parse(data);
-//     id = LIST.length; // set the id to the last one in the list
-//     loadList(LIST);
-// }
-// else {
-//     // if data isn't empty
-//     LIST = [];
-//     id = 0;
-// }
-
-// function loadList(arr) {
-//     arr.forEach(element => {
-
-//     });
-// }
-
-
 function add() {
-    let inputVal = $("#input").val();
 
-    if (inputVal == '') return;
+    if ($("#input").val() == '') return; // No empty inputs on main input
 
-    else {
-        let li = document.createElement("li");
-        let listInput = document.createElement("input");
-        listInput.classList.add("li-input");
-        listInput.value = inputVal;
-        listInput.readOnly = true;
+    let li = document.createElement("li");
+    let listInput = document.createElement("input");
+    listInput.classList.add("li-input");
+    listInput.value = $("#input").val();
+    listInput.readOnly = true;
 
-        let circleIcon = document.createElement("i");
-        circleIcon.classList.add("fa", "fa-circle-thin", "icon", "toggle-icon");
+    let circleIcon = document.createElement("i");
+    circleIcon.classList.add("fa", "fa-circle-thin", "icon", "toggle-icon");
 
-        let checkedIcon = document.createElement("i");
-        checkedIcon.classList.add("fas", "fa-check-circle", "icon");
+    let checkedIcon = document.createElement("i");
+    checkedIcon.classList.add("fas", "fa-check-circle", "icon");
 
-        let editIcon = document.createElement("i");
-        editIcon.classList.add("fas", "fa-edit", "edit", "icon");
+    let editIcon = document.createElement("i");
+    editIcon.classList.add("fas", "fa-edit", "edit", "icon");
 
-        let clearIcon = document.createElement("i");
-        clearIcon.classList.add("fas", "fa-trash-alt", "clear", "icon");
+    let clearIcon = document.createElement("i");
+    clearIcon.classList.add("fas", "fa-trash-alt", "clear", "icon");
 
-        li.classList.add("li");
-        li.append(circleIcon);
-        li.append(editIcon);
-        li.append(clearIcon);
-        li.append(listInput);
+    li.classList.add("li");
+    li.append(circleIcon);
+    li.append(editIcon);
+    li.append(clearIcon);
+    li.append(listInput);
 
-        ulTodo.append(li);
-
-        input.val("");
-
-        // LIST.push({
-        //     name: inputVal,
-        //     id: id
-        // });
-
-        // localStorage.setItem("todo", JSON.stringify(LIST));
-        // id++;
-    }
+    $("#ul-todo").append(li);
+    $("#input").val(""); // Clear the input on pressing "Add" or Enter key
 }
 
-// Create a new list item when clicking the "Add" button
+// Create a new list item when clicking "Add" button
 btnAdd.click(function () {
     add();
 });
 
-// Adding list with Enter key
-input.keypress(function (e) {
+// Adding list item with Enter key
+$("#input").keypress(function (e) {
     if (e.which === 13) {
         e.preventDefault();
         btnAdd.click();
         return false;
     }
 });
-
 //  On Reset Button Click
-btnReset.click(function () {
+$("#btnReset").click(function () {
     let list = document.getElementsByClassName("li");
+
+    if (confirm("Are you sure?") == false) return;
 
     // Remove the 1st element, until there is no 1st element
     while (list[0]) {
@@ -99,9 +62,7 @@ btnReset.click(function () {
 });
 
 $("#ul-completed").click(function (e) {
-    let closestLi = $(e.target.closest('li'));
     let closestInput = $(e.target).closest('li').find('.li-input');
-
 
     $(".li-input").keypress(function (e) {
         if (e.which === 13) {
@@ -116,19 +77,18 @@ $("#ul-completed").click(function (e) {
         closestInput.addClass("strike-text font-italic");
         closestInput.attr("readonly", true);
     });
-
-
 });
 
 $("ul").click(function (e) {
-    // Removes the 'li' if you click the "x" button
+    let closestLi = $(e.target.closest('li'));
+    let closestInput = $(e.target).closest('li').find('.li-input');
+
+    // Removes the 'li' if you click the "trash icon" button
     if (e.target.classList.contains('clear')) {
         e.target.parentElement.remove();
     }
 
     if (e.target.classList.contains('edit')) {
-        let closestLi = $(e.target.closest('li'));
-        let closestInput = $(e.target).closest('li').find('.li-input');
 
         if (closestInput.attr("readonly"), true) {
             closestInput.removeAttr("readonly");
@@ -137,15 +97,13 @@ $("ul").click(function (e) {
             closestInput.attr("readonly", true);
         }
 
-        //If its in ul-completed
+        //If its in '#ul-completed'
         if (closestInput.hasClass("strike-text") && closestLi.parent("#ul-completed")) {
             closestInput.removeClass("strike-text font-italic");
         }
-
         else if (closestLi.parent("#ul-todo")) {
             closestInput.removeClass("strike-text font-italic");
         }
-
         else if (closestLi.parent("#ul-completed")) {
             closestInput.addClass("strike-text font-italic");
         }
@@ -159,15 +117,13 @@ $("ul").click(function (e) {
             }
         });
 
-        $(".li-input").focusout(function (e) {
+        $(".li-input").focusout(function () {
             closestInput.attr("readonly", true);
         });
     }
 
 
     if ($(e.target).is("i") && $(e.target).hasClass('toggle-icon')) {
-        let closestLi = $(e.target.closest('li'));
-        let closestInput = $(e.target).closest('li').find('.li-input');
         let closestIcon = $(e.target).closest('i');
         let unchecked = 'fa fa-circle-thin icon';
         let checked = 'fas fa-check-circle icon';
@@ -198,14 +154,3 @@ $("ul").click(function (e) {
         }
     };
 });
-
-$.fn.hasAttr = function (name) {
-    return this.attr(name) !== undefined;
-}
-
-// jQuery.fn.toggleAttr = function (attr) {
-//     return this.each(function () {
-//         var $this = $(this);
-//         $this.attr(attr) ? $this.removeAttr(attr) : $this.attr(attr, attr);
-//     });
-// };
